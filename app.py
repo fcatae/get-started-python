c5ddddac54e1be5c4128e9466c15440f69cb68f1
Fabricio Catae <fcatae@microsoft.com>
2018-06-12 10:07:15 -0300

    Add static content

--- app.py
+++ app.py
@@ -1,9 +1,23 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import (
+    Flask,
+    render_template
+)
+
+app = Flask(__name__, template_folder="static")
 
 @app.route("/")
 def hello():
     return "Hello World!"
+
+@app.route('/home')
+def home():
+    """
+    This function just responds to the browser ULR
+    localhost:5000/
+
+    :return:        the rendered template 'home.html'
+    """
+    return render_template('home.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
